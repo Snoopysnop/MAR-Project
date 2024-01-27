9b90f3b7af2493870e54adcd9496a2fe8f894743
lribeirogome <lise.gomes@etudiant.univ-rennes1.fr>
2024-01-27 11:19:31 +0100

    add key and door to interact with + first hostage + menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
